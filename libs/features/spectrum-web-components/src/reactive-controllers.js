db7c5ed37ac3a0b31e0c96df49cc5d3d2c3494c1
Mira <myrosvas@gmail.com>
2024-10-16 10:18:20 +0200

    added systemResolverUpdatedSymbol

--- libs/features/spectrum-web-components/src/reactive-controllers.js
+++ libs/features/spectrum-web-components/src/reactive-controllers.js
@@ -2,4 +2,4 @@ export * from '@spectrum-web-components/reactive-controllers/src/index.js';
 export * from '@spectrum-web-components/reactive-controllers/src/ElementResolution.js';
 export * from '@spectrum-web-components/reactive-controllers/src/DependencyManger.js';
 export { PendingStateController } from '@spectrum-web-components/reactive-controllers/src/PendingState.js';
-export { SystemResolutionController } from '@spectrum-web-components/reactive-controllers/src/SystemContextResolution.js';
+export { SystemResolutionController, systemResolverUpdatedSymbol } from '@spectrum-web-components/reactive-controllers/src/SystemContextResolution.js';
