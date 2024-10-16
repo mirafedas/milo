55b1e8d8163929db6a254ab0ad7fd4024bfef179
Mira <myrosvas@gmail.com>
2024-10-16 10:17:04 +0200

    added SystemResolutionController

--- libs/features/spectrum-web-components/src/reactive-controllers.js
+++ libs/features/spectrum-web-components/src/reactive-controllers.js
@@ -2,3 +2,4 @@ export * from '@spectrum-web-components/reactive-controllers/src/index.js';
 export * from '@spectrum-web-components/reactive-controllers/src/ElementResolution.js';
 export * from '@spectrum-web-components/reactive-controllers/src/DependencyManger.js';
 export { PendingStateController } from '@spectrum-web-components/reactive-controllers/src/PendingState.js';
+export { SystemResolutionController } from '@spectrum-web-components/reactive-controllers/src/SystemContextResolution.js';
